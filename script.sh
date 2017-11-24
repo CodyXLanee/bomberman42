a4f9fc09b8e802699b03a61ce7ec4fa8a3ddcc75
Eliott GABOREAU <egaborea@e3r2p12.42.fr>
2017-11-24 13:02:37 +0100

    install script update

--- script.sh
+++ script.sh
@@ -2,4 +2,6 @@
 brew install glm
 brew install sdl2
 brew install freetype
-curl https://raw.githubusercontent.com/nothings/stb/master/stb_image.h >> libs/stb_image.h
+brew install pkg-config
+brew install assimp
+curl https://raw.githubusercontent.com/nothings/stb/master/stb_image.h > libs/stb_image.h

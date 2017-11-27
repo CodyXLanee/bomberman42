fffde24e9b5535fafcf71f792d910811bf85d6ba
4quet <louis.fourquet@gmail.com>
2017-11-27 09:35:34 +0100

    Added rapidjson to install script

--- script.sh
+++ script.sh
@@ -4,4 +4,5 @@ brew install sdl2
 brew install freetype
 brew install pkg-config
 brew install assimp
+brew install rapidjson
 curl https://raw.githubusercontent.com/nothings/stb/master/stb_image.h > libs/stb_image.h

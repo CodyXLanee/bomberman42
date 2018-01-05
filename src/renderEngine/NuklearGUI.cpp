8a1c4a3c01d3f286e8fcb74335bfb26c2cd54119
Adrien BOSSI <abossi@e3r3p13.42.fr>
2018-01-05 18:51:49 +0100

    add a level in campaign mode

--- src/renderEngine/NuklearGUI.cpp
+++ src/renderEngine/NuklearGUI.cpp
@@ -524,7 +524,7 @@ void    NuklearGUI::renderLevelSelection() {
         }
         if (nk_button_label(ctx, "Next"))
         {
-            if (level < Level::FIVE)
+            if (level < Level::SIX)
             {
                 level = static_cast<Level::Enum>(level + 1);
                 game_params.set_level(level);
